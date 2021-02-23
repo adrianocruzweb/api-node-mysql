8ee121a5cb7147896ab92e7eeaf259b2b9a4d1c7
adrianocruzweb <adrianocruzweb@hotmail.com>
2021-02-22 22:12:26 -0400

    make it better conf port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const routes = require('./routes')
+let port = process.env.PORT || 8080;
 
 require('./database')
 
@@ -9,4 +10,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3030)
+app.listen(port)
